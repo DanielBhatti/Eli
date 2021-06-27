5ff1fd24ef8b7cdab4bb90ec5a119daf8a19502c
Daniel Bhatti <danielbbhatti@gmail.com>
2021-06-27 19:50:50 -0400

    Also added test for DateTime-double plotting.

--- CommonTest/PlotViewTest.cs
+++ CommonTest/PlotViewTest.cs
@@ -42,10 +42,9 @@ namespace CommonTest
             Assert.AreEqual(true, true);
         }
 
-        [Test, Apartment(System.Threading.ApartmentState.STA)]
+        //[Test, Apartment(System.Threading.ApartmentState.STA)]
         public void PlotDoubleDataThroughProperties()
         {
-
             List<double> xData = new List<double>();
             List<double> yData = new List<double>();
             for (int i = 0; i < 10; i++)
@@ -89,6 +88,33 @@ namespace CommonTest
             plotControl.AddAxis(Position.Bottom, "Bottom", AxisType.DateTime);
             plotControl.AddAxis(Position.Left, "Left");
             plotControl.AddSeries(xData, yData, "TestName");
+
+            csu.ShowControl(plotControl);
+
+            Assert.AreEqual(true, true);
+        }
+
+        [Test, Apartment(System.Threading.ApartmentState.STA)]
+        public void PlotDateTimeDoubleDataThroughProperties()
+        {
+            List<DateTime> xData = new List<DateTime>();
+            List<double> yData = new List<double>();
+            for (int i = 0; i < 10; i++)
+            {
+                xData.Add(new DateTime(2021, 1, i + 1));
+                yData.Add(Math.Pow(2, i));
+            }
+
+            string title = "Title";
+
+            PlotControl plotControl = new PlotControl();
+            plotControl.Title = title;
+
+            Axis bottomAxis = new Axis("Bottom", Position.Bottom, AxisType.DateTime);
+            Axis leftAxis = new Axis("Left", Position.Left, AxisType.Linear);
+            PlotData<DateTime, double> plotData = new PlotData<DateTime, double>(xData, yData, "Data", Color.Black, LineStyle.Solid, 2.0);
+            plotControl.PlotAxes = new ObservableCollection<Axis>() { bottomAxis, leftAxis };
+            plotControl.PlotData = new ObservableCollection<object>() { plotData };
 
             csu.ShowControl(plotControl);
 

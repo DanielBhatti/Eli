d055a9d8a21f46617a36fafd6602b3e0e0a56595
Daniel Bhatti <danielbbhatti@gmail.com>
2021-07-16 13:33:19 -0400

    Added HashMaker class for creating hases.

---
+++ Common/Security/HashMaker.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Common.Security
+{
+    public class HashMaker
+    {
+        public string GenerateSalt()
+        {
+            byte[] bytes = new byte[12];
+            RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
+            rng.GetBytes(bytes);
+            return Convert.ToBase64String(bytes);
+        }
+
+        public string GenerateHash(byte[] bytes, byte[] salt, int iterations = 10000)
+        {
+            Rfc2898DeriveBytes result = new Rfc2898DeriveBytes(bytes, salt, iterations);
+            return Convert.ToBase64String(result.GetBytes(24));
+        }
+
+        public string GenerateHash(string password, string salt)
+        {
+            return GenerateHash(Encoding.UTF8.GetBytes(password), Encoding.UTF8.GetBytes(salt));
+        }
+    }
+}

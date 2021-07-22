41a865b877dc8d6a3dbe0143fd4f4a7cdaa8fb6f
Daniel Bhatti <danielbbhatti@gmail.com>
2021-07-21 23:43:21 -0400

    Added a WIP control and useful ValueConverter.

---
+++ Common/Wpf/BooleanToVisibilityConverter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Common.Wpf
+{
+    public class BooleanToVisibilityConverter : IValueConverter
+    {
+        public bool NegateValue { get; set; } = false;
+        public bool ConvertToHidden { get; set; } = false;
+
+        public Visibility HiddenOrCollapsed
+        { 
+            get
+            {
+                if (ConvertToHidden) return Visibility.Hidden;
+                else return Visibility.Collapsed;
+            } 
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is null || value.GetType() != typeof(bool)) return null;
+
+            if ((bool)value == true)
+            {
+                if (!NegateValue) return (object)Visibility.Visible;
+                else return (object)HiddenOrCollapsed;
+            }
+            else
+            {
+                if (!NegateValue) return (object)HiddenOrCollapsed;
+                else return (object)Visibility.Visible;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is null || value.GetType() != typeof(Visibility)) return null;
+
+            if ((Visibility)value == Visibility.Visible)
+            {
+                if (!NegateValue) return (object)true;
+                else return (object)false;
+            }
+            else
+            {
+                if (!NegateValue) return (object)false;
+                else return (object)true;
+            }
+        }
+    }
+}
---
+++ Common/Wpf/SearchableListControl.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Common.Wpf
+{
+    /// <summary>
+    /// Interaction logic for SearchableListControl.xaml
+    /// </summary>
+    public partial class SearchableListControl : ListView
+    {
+        public DependencyProperty DisplayTextProperty = DependencyProperty.Register(
+            nameof(DisplayText), typeof(string), typeof(SearchableListControl), new PropertyMetadata());
+        public string DisplayText
+        {
+            get => (string)GetValue(DisplayTextProperty);
+            set => SetValue(DisplayTextProperty, value);
+        }
+
+        public ICommand DeleteItemCommand;
+
+        public SearchableListControl()
+        {
+            InitializeComponent();
+            throw new NotImplementedException();
+        }
+
+        public void DeleteItem()
+        {
+
+        }
+    }
+}

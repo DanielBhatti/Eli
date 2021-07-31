f0205f0eb9d710e4e8604d40fc66f09c88d9787e
Daniel Bhatti <danielbbhatti@gmail.com>
2021-07-31 16:07:01 -0400

    Some minor work, need to figure out how list binding works and how to properly affect the children elements.

--- Common/Wpf/SearchableListControl.xaml.cs
+++ Common/Wpf/SearchableListControl.xaml.cs
@@ -8,7 +8,7 @@ namespace Common.Wpf
     /// <summary>
     /// Interaction logic for SearchableListControl.xaml
     /// </summary>
-    public partial class SearchableListControl : ListView
+    public partial class SearchableListControl : UserControl
     {
         public DependencyProperty DisplayTextProperty = DependencyProperty.Register(
             nameof(DisplayText), typeof(string), typeof(SearchableListControl), new PropertyMetadata());
@@ -16,6 +16,15 @@ namespace Common.Wpf
         {
             get => (string)GetValue(DisplayTextProperty);
             set => SetValue(DisplayTextProperty, value);
+        }
+
+        public DependencyProperty SearchableListItemSourceProperty = DependencyProperty.Register(
+            nameof(SearchableListItemSource), typeof(ItemCollection), typeof(SearchableListControl), new PropertyMetadata());
+
+        public ItemCollection SearchableListItemSource
+        {
+            get => (ItemCollection)GetValue(SearchableListItemSourceProperty);
+            set => SetValue(SearchableListItemSourceProperty, value);
         }
 
         public ICommand DeleteItemCommand;

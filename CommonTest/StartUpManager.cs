1b96c5b6d662568ebb6b94d6bc4671ef069e697c
Daniel Bhatti <danielbbhatti@gmail.com>
2021-10-31 21:24:13 -0400

    Removed some old WPF dependencies.

--- CommonTest/StartUpManager.cs
+++ CommonTest/StartUpManager.cs
@@ -1,23 +1,12 @@
 ﻿using NUnit.Framework;
 using System.IO;
-using System.Windows;
-using System.Windows.Controls;
 
 namespace CommonTest
 {
     [Apartment(System.Threading.ApartmentState.STA)]
     public class StartUpManager
     {
-        public static string TestDirectory { get; } = @"C:\Users\bhatt\Programming\Common\CommonTest\";
+        public static string TestDirectory { get; } = @"C:\Users\bhatt\Repositories\Common\CommonTest\";
         public static string ResourcesDirectory { get; } = Path.Combine(TestDirectory, "Resources");
-
-        public void ShowControl(Control control)
-        {
-            Window window = new Window();
-            window.Content = control;
-
-            Application app = new Application();
-            app.Run(window);
-        }
     }
 }

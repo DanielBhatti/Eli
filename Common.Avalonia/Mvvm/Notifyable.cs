d0a8dd0edf18c781109fc62eebf87579a121ee41
Daniel Bhatti <danielbbhatti@gmail.com>
2021-11-02 16:03:16 -0400

    Added Notifyable wrapper class.  Intent is to wrap around a type and implement the NotifyPropertyChanged for Avalonia.

---
+++ Common.Avalonia/Mvvm/Notifyable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Threading;
+using ReactiveUI;
+
+namespace Common.Avalonia.Mvvm
+{
+    public class Notifyable<T> : ReactiveObject
+    {
+        private T _value = default(T);
+        public T Value
+        {
+            get => _value;
+            set => this.RaiseAndSetIfChanged(ref _value, value);
+        }
+
+        public static implicit operator T(Notifyable<T> notifyable)
+        {
+            return notifyable.Value;
+        }
+    }
+}

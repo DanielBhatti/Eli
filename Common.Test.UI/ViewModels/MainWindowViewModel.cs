88497e158b04438c1f42eda4ff43b57e977f4a6c
Daniel Bhatti <danielbbhatti@gmail.com>
2021-11-25 22:23:34 -0500

    Made some changes, code isn't running anymore, don't know why.

--- Common.Test.UI/ViewModels/MainWindowViewModel.cs
+++ Common.Test.UI/ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Common.Test.UI.ViewModels
@@ -7,5 +8,17 @@ namespace Common.Test.UI.ViewModels
     public class MainWindowViewModel : ViewModelBase
     {
         public ScatterPlotViewModel ScatterPlotViewModel { get; } = new ScatterPlotViewModel();
+
+        public void TestLargeDataSet(int n)
+        {
+            Random random = new Random();
+            ScatterPlotViewModel.XData = Enumerable.Range(0, n).Select(i => (double)i).ToArray();
+            ScatterPlotViewModel.YData = Enumerable.Repeat(0, n).Select(i => 100 * random.NextDouble()).ToArray();
+        }
+
+        public MainWindowViewModel()
+        {
+            //TestLargeDataSet(100);
+        }
     }
 }

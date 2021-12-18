46ea3b1fc0d01db3f5a33c9c56ec3c39db9ce6fc
Daniel Bhatti <danielbbhatti@gmail.com>
2021-12-17 21:35:26 -0500

    New interface.

---
+++ Common.Avalonia/Mvvm/IPersistable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Common.Avalonia.Mvvm
+{
+    public interface IPersistable
+    {
+        string Serialize(object o, Type type);
+
+        object Deserialize(string s, Type type);
+
+        void Save();
+
+        void Load();
+    }
+}

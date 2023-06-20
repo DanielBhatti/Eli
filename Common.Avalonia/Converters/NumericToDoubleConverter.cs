07629538709667be06ac611b0bd42ed2e10e3921
Daniel Bhatti <danielbbhatti@gmail.com>
2023-06-19 21:03:00 -0400

    Finally fixed the SetValueControl.

--- Common.Avalonia/Converters/NumericToDoubleConverter.cs
+++ Common.Avalonia/Converters/NumericToDoubleConverter.cs
@@ -8,7 +8,7 @@ public class NumericToDoubleConverter : IValueConverter
 {
     public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
     {
-        if(value != null)
+        if(!String.IsNullOrEmpty(value as string))
         {
             return System.Convert.ToDouble(value);
         }

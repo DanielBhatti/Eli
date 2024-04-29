efac3bde4308be735b2b80288e8c2758cd2a984c
Daniel Bhatti <dbhatti@annaly.com>
2024-04-29 16:27:20 -0400

    Started working on numerical analysis module.  Added RK4 but seems to be buggy.

---
+++ Eli.Math.Fs.Test/NumericalAnalysisTest.fs
@@ -0,0 +1,41 @@
+﻿namespace Eli.Math.Fs.Test
+
+open NUnit.Framework
+open Eli.Math.Fs
+open System
+
+module NumericalAnalysisTest =
+    let tolerance = 1e-5
+
+    [<Test>]
+    let ``Test rk4Integrate with exponential growth``() =
+        let f x y = y
+        let xStart = 0.0
+        let yStart = 1.0
+        let xEnd = 1.0
+        let h = 0.1
+        let result = NumericalAnalysis.rk4Integrate f xStart yStart xEnd h
+        let expected = Math.Exp(xEnd)
+        Assert.AreEqual(expected, result, tolerance)
+
+    [<Test>]
+    let ``Test rk4Integrate with small step size``() =
+        let f x y = y
+        let xStart = 0.0
+        let yStart = 1.0
+        let xEnd = 1.0
+        let h = 0.0001
+        let result = NumericalAnalysis.rk4Integrate f xStart yStart xEnd h
+        let expected = Math.Exp(xEnd)
+        Assert.AreEqual(expected, result, tolerance)
+
+    [<Test>]
+    let ``Test rk4Integrate on a known polynomial solution``() =
+        let f x y = 3.0 * x * x
+        let xStart = 0.0
+        let yStart = 0.0
+        let xEnd = 1.0
+        let h = 0.1
+        let result = NumericalAnalysis.rk4Integrate f xStart yStart xEnd h
+        let expected = xEnd ** 3.0
+        Assert.AreEqual(expected, result, tolerance)
---
+++ Eli.Math.Fs/NumericalAnalysis.fs
@@ -0,0 +1,18 @@
+﻿namespace Eli.Math.Fs
+
+module NumericalAnalysis =
+    let rk4Step f' x y h =
+        let k1 = f' x y
+        let k2 = f' (x + h / 2.0) (y + h / 2.0 * k1)
+        let k3 = f' (x + h / 2.0) (y + h / 2.0 * k2)
+        let k4 = f' (x + h) (y + h * k3)
+
+        y + h / 6.0 * (k1 + 2.0 * k2 + 2.0 * k3 + k4)
+
+    let rk4Integrate f' xStart yStart xEnd h =
+        let mutable x = xStart
+        let mutable y = yStart
+        while x < xEnd do
+            y <- rk4Step f' h x y
+            x <- x + h
+        y

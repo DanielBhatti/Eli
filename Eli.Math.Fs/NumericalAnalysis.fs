c0c50276254097adc28db02492af8fb2828aba1a
Daniel Bhatti <dbhatti@annaly.com>
2024-04-30 21:20:17 -0400

    Refactor.

--- Eli.Math.Fs/NumericalAnalysis.fs
+++ Eli.Math.Fs/NumericalAnalysis.fs
@@ -1,44 +1,40 @@
 ﻿namespace Eli.Math.Fs
 
 module NumericalAnalysis =
-    let rk4Step f' x y h =
-        let k1 = f' x y
-        let k2 = f' (x + h / 2.0) (y + h / 2.0 * k1)
-        let k3 = f' (x + h / 2.0) (y + h / 2.0 * k2)
-        let k4 = f' (x + h) (y + h * k3)
-        y + h / 6.0 * (k1 + 2.0 * k2 + 2.0 * k3 + k4)
+    let rec integrate methodStep f' tStart xStart tEnd h =
+        if tStart >= tEnd then xStart
+        else integrate methodStep f' (tStart + h) (methodStep f' tStart xStart h) tEnd h
 
-    let rec rk4Integrate f' xStart yStart xEnd h =
-        if xStart >= xEnd then yStart
-        else rk4Integrate f' (xStart + h) (rk4Step f' xStart yStart h) xEnd h
+    let rk4Step f' t x h =
+        let k1 = f' t x
+        let k2 = f' (t + h / 2.0) (x + h / 2.0 * k1)
+        let k3 = f' (t + h / 2.0) (x + h / 2.0 * k2)
+        let k4 = f' (t + h) (x + h * k3)
+        x + h / 6.0 * (k1 + 2.0 * k2 + 2.0 * k3 + k4)
+
+    let rec rk4Integrate f' tStart xStart tEnd h = integrate rk4Step f' tStart xStart tEnd h
 
     let eulerStep f' (x: float) y h = y + h * f' x y
 
-    let rec eulerIntegrate f' xStart yStart xEnd h =
-        if xStart >= xEnd then yStart
-        else eulerIntegrate f' (xStart + h) (eulerStep f' xStart yStart h) xEnd h
+    let rec eulerIntegrate f' tStart xStart tEnd h = integrate eulerStep f' tStart xStart tEnd h
 
-    let heunStep f' x y h =
-        let yEuler = eulerStep f' x y h
-        let yCorrected = y + h / 2.0 * (f' x y + f' (x + h) yEuler)
+    let heunStep f' t x h =
+        let yEuler = eulerStep f' t x h
+        let yCorrected = x + h / 2.0 * (f' t x + f' (t + h) yEuler)
         yCorrected
 
-    let rec heunIntegrate f' xStart yStart xEnd h =
-        if xStart >= xEnd then yStart
-        else heunIntegrate f' (xStart + h) (heunStep f' xStart yStart h) xEnd h
+    let rec heunIntegrate f' tStart xStart tEnd h = integrate heunStep f' tStart xStart tEnd h
+    
+    let midpointStep f' t x h =
+        let midPoint = x + h / 2.0 * f' t x
+        x + h * f' (t + h / 2.0) midPoint
 
-    let midpointStep f' x y h =
-        let midPoint = y + h / 2.0 * f' x y
-        y + h * f' (x + h / 2.0) midPoint
+    let rec midpointIntegrate f' tStart xStart tEnd h = integrate midpointStep f' tStart xStart tEnd h
 
-    let rec midpointIntegrate f' xStart yStart xEnd h =
-        if xStart >= xEnd then yStart
-        else midpointIntegrate f' (xStart + h) (midpointStep f' xStart yStart h) xEnd h
-
-    let rec tracePath f' (xStart: float) yStart xEnd h path method =
-        if xStart >= xEnd then
+    let rec tracePath methodStep f' (tStart: float) xStart tEnd h path =
+        if tStart >= tEnd then
             List.rev path
         else
-            let xNext = xStart + h
-            let yNext = method f' xStart yStart h
-            tracePath f' xNext yNext xEnd h ((xNext, yNext)::path) method
+            let xNext = tStart + h
+            let yNext = methodStep f' tStart xStart h
+            tracePath methodStep f' xNext yNext tEnd h ((xNext, yNext)::path)

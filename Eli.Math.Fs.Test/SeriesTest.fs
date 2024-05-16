dcb413a995ea2a3667664ff11ed6c63686627872
Daniel Bhatti <dbhatti@annaly.com>
2024-05-16 14:39:12 -0400

    Updated Series test cases.

--- Eli.Math.Fs.Test/SeriesTest.fs
+++ Eli.Math.Fs.Test/SeriesTest.fs
@@ -11,16 +11,16 @@ module SeriesTest =
         let x0 = 0.0
         let n = 3
         let h = 1e-5
-        let series = Series.seriesExpansion f x0 n h
+        let series = Series.taylorExpansion f x0 n h
         Assert.AreEqual(f 1.0, series 1.0)
 
     [<Test>]
     let ``Test series expansion for a quadratic function``() =
-        let f x = x * x
+        let f x = 2.0 * x * x + 3.0 * x + 4.0
         let x0 = 1.0
         let n = 3
         let h = 1e-5
-        let series = Series.seriesExpansion f x0 n h
+        let series = Series.taylorExpansion f x0 n h
         Assert.AreEqual(f 2.0, series 2.0)
 
     [<Test>]
@@ -29,5 +29,5 @@ module SeriesTest =
         let x0 = 1.0
         let n = 4
         let h = 1e-5
-        let series = Series.seriesExpansion f x0 n h
+        let series = Series.taylorExpansion f x0 n h
         Assert.AreEqual(f 2.0, series 2.0)
